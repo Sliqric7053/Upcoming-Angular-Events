5914a1017f1459e51c11d999557e9d046f355ec1
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2018-12-23 18:03:51 +0200

    feat/setup app shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import 'core-js/es7/reflect';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
+import { NavComponent } from './nav/nav.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventThumbnailComponent,
+    NavComponent,
+    EventsListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [EventService, ToastrService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
