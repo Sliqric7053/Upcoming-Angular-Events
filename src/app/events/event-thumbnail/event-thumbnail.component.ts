62ae63bc298cdff67353f1424ef9448d6ccf38da
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2018-12-23 18:05:01 +0200

    feat/add events thumbnail component

---
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.scss'],
+})
+export class EventThumbnailComponent implements OnInit {
+  @Input() event: any;
+  @Output() emitClick = new EventEmitter();
+  text = 'local/template var binding';
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  clickEvent() {
+    this.emitClick.emit(this.event);
+  }
+
+  getStartTimeClass() {
+    // [ngClass] expects a returned object OR string or array of strings
+
+    // option 1
+    // const isEarlyStart = this.event && this.event.time === '8:00 am';
+    // return { green: isEarlyStart, bold: isEarlyStart };
+
+    // option 2
+    // if (this.event && this.event.time === '8:00 am') {
+    //   return 'green bold';
+    // }
+    // return '';
+
+    // option 3
+    if (this.event && this.event.time === '8:00 am') {
+      return ['green', 'bold'];
+    }
+    return [];
+  }
+}
