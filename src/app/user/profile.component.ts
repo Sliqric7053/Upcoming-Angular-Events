b3f0501e222e989b97f618d82ac94f9522239c4d
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2019-01-04 11:14:32 +0200

    feat/add reactive form controls

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
-  constructor() {}
+  profileForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(private auth: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    const firstName = new FormControl(this.auth.currentUser.firstName);
+    const lastName = new FormControl(this.auth.currentUser.lastName);
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName,
+    });
+  }
+
+  cancel() {
+    this.router.navigate(['/events']);
+  }
+
+  saveProfile(formValues) {
+    this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
+    this.router.navigate(['/events']);
+  }
 }
--- src/app/user/profile.module.ts
+++ src/app/user/profile.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UserProfileComponent } from './profile.component';
 import { UserProfileRoutingModule } from './profile-routes.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports: [CommonModule, UserProfileRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    UserProfileRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   declarations: [UserProfileComponent, LoginComponent],
 })
 export class UserProfileModule {}
