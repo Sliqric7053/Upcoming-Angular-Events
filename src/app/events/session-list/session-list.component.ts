c1c0981de38cb08b42726148c2b44c4dc328edb5
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2019-01-07 11:38:43 +0200

    feat/add event filtering logic

--- src/app/events/session-list/session-list.component.ts
+++ src/app/events/session-list/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared/event.model';
 
 @Component({
@@ -6,10 +6,27 @@ import { ISession } from '../shared/event.model';
   templateUrl: './session-list.component.html',
   styleUrls: ['./session-list.component.scss'],
 })
-export class SessionListComponent implements OnInit {
-  @Input() sessions: ISession;
+export class SessionListComponent implements OnChanges {
+  @Input() sessions: ISession[];
+  @Input() filterBy: string;
+  visibleSessions: ISession[] = [];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnChanges() {
+    // do not handle if sessions are not yet set
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  filterSessions(filter) {
+    if (filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      });
+    }
+  }
 }
