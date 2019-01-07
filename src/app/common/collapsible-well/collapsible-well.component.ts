410a7bc347e4a78aff0dac7a14a95cc742a68df0
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2019-01-07 09:02:45 +0200

    feat/refactor and add multiple content projections

--- src/app/common/collapsible-well/collapsible-well.component.ts
+++ src/app/common/collapsible-well/collapsible-well.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./collapsible-well.component.scss'],
 })
 export class CollapsibleWellComponent implements OnInit {
-  @Input() title: string;
   visible = true;
 
   constructor() {}
