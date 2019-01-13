8de87fe55e381d30b6845ee76350c45f7c2002a2
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2019-01-13 07:30:20 +0200

    feat/add upvote component

---
+++ src/app/events/upvote/upvote.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-upvote',
+  templateUrl: './upvote.component.html',
+  styleUrls: ['./upvote.component.scss'],
+})
+export class UpvoteComponent implements OnInit {
+  @Input() count: number;
+  @Input() voted: boolean;
+  @Output() vote = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onClick() {
+    this.vote.emit({});
+  }
+}
