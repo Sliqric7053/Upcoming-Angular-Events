d497bab137d6d2c8fb00367c5850d77e8daa300f
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2019-01-13 17:17:58 +0200

    feat/add http get method to fetch event data from the server

--- src/app/events/events-list-resolver.service.ts
+++ src/app/events/events-list-resolver.service.ts
@@ -9,6 +9,6 @@ export class EventsListResolverService implements Resolve<any> {
   constructor(private eventService: EventService) {}
 
   resolve(): Observable<any> | Promise<any> | any {
-    return this.eventService.getEvents().pipe(map(events => events));
+    return this.eventService.getEvents();
   }
 }
