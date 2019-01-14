410bcc1ad6bdadf8f66d7e7d9b93d1c77bb81176
Ricci Rich <riccirich@Riccis-MacBook-Pro.local>
2019-01-14 21:16:49 +0200

    refactor/authenticate user on the server

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,21 +1,37 @@
 import { Injectable } from '@angular/core';
 import { IUser } from './user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   currentUser: IUser;
 
-  loginUser(username: string, password: string) {
-    this.currentUser = {
-      id: 1,
-      userName: username,
-      firstName: 'John',
-      lastName: 'Papa',
+  loginUser(username: string, password: string): Observable<IUser | boolean> {
+    const options = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
     };
+    return this.http
+      .post<IUser>(
+        '/api/login',
+        { username: username, password: password },
+        options
+      )
+      .pipe(
+        tap(data => {
+          this.currentUser = <IUser>data['user'];
+        })
+      )
+      .pipe(
+        catchError(error => {
+          return of(false);
+        })
+      );
   }
 
   isAuthenticated() {
